66fdb507d352caf3015fb7cf7b98879e623600f5
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2024-05-31 17:10:17 +0700

    config.py

--- antigcast/config.py
+++ antigcast/config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6726190227:AAE_6tUiU3MnMchzVXKWkeNKkG9LpJGJA7c")
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
-APP_ID = int(os.environ.get("APP_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+APP_ID = int(os.environ.get("APP_ID", "26477680"))
+API_HASH = os.environ.get("API_HASH", "b0d8504752cc1ecf52009ece2bdef0b8")
 
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", ""))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002075430848"))
 
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "wildannganteng")
@@ -19,7 +19,7 @@ BROADCAST_AS_COPY = True
 
 PLUG = dict(root="antigcast/plugins")
 
-OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "").split())]
+OWNER_ID = [int(x) for x in (os.environ.get("OWNER_ID", "5779185981").split())]
 OWNER_NAME = os.environ.get("OWNER_NAME", "mhmdwldnnnn")
 
 
